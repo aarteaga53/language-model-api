a4f0b0b299e49307c32ccc92ce64a0ed05e67797
aarteaga53 <andrewarteaga123@gmail.com>
2023-03-09 14:09:12 -0800

    initializing git repo

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import random
+import json
+
+import torch
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def start():
+    return 'Flask App'
+
+@app.route('/hello')
+def hello():
+    return jsonify(message='Hello, World!')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    req_data = request.get_json()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    with open('intents.json', 'r') as json_data:
+        intents = json.load(json_data)
+
+    FILE = "data.pth"
+    data = torch.load(FILE)
+
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    all_words = data['all_words']
+    tags = data['tags']
+    model_state = data["model_state"]
+
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
+
+    sentence = req_data['sentence']
+    response = ''
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                response = random.choice(intent['responses'])
+    else:
+        response = 'I do not understand...'
+
+    return jsonify({"sentence": response})
+
+if __name__ == '__main__':
+    app.run()
